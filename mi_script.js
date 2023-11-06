745235b0d5ff0b83a124e24a89f0942874ada185
sfeilbogen <88330260+sfeilbogen@users.noreply.github.com>
2023-11-06 20:51:56 -0300

    cambiooos

---
+++ mi_script.js
@@ -0,0 +1,25 @@
+function CarritoDeComprasApp()  {
+    compras=[]
+   
+
+}
+
+function AgregoUsuario(Email,Nombre){
+    let usuario={
+        correoElectronico : document.getElementById(""),
+        Nombre: document.getElementById("")
+    }
+
+    compras.push(usuario)
+
+}
+function AgregoCarrito(Modelo,Precio){
+    let compra={
+        modelo:Modelo,
+        precio:Precio
+    }
+    compras.push(compra)
+}
+    
+   
+

a6675b7e492a8e580900b8c5a068980f6ecfd14c
sfeilbogen <88330260+sfeilbogen@users.noreply.github.com>
2023-11-09 20:05:34 -0300

    cambios

--- mi_script.js
+++ mi_script.js
@@ -1,25 +1,33 @@
-function CarritoDeComprasApp()  {
-    compras=[]
-   
+const compras=[]
 
-}
-
-function AgregoUsuario(Email,Nombre){
-    let usuario={
-        correoElectronico : document.getElementById(""),
-        Nombre: document.getElementById("")
-    }
-
-    compras.push(usuario)
-
-}
-function AgregoCarrito(Modelo,Precio){
+function AgregoCarrito(){
     let compra={
-        modelo:Modelo,
-        precio:Precio
+        modelo:document.getElementById("modelo1").textContent,
+        precio:document.getElementById("precio1").textContent
     }
-    compras.push(compra)
+    compras.push(compra);
+    mostrarEnCarrito(compra);
 }
-    
-   
+function AgregoCarrito2(){
+    let compra={
+        modelo:document.getElementById("modelo2").textContent,
+        precio:document.getElementById("precio2").textContent
+    }
+    compras.push(compra);
+    mostrarEnCarrito(compra);
+}
+function AgregoCarrito3(){
+    let compra={
+        modelo:document.getElementById("modelo3").textContent,
+        precio:document.getElementById("precio3").textContent
+    }
+    compras.push(compra);
+    mostrarEnCarrito(compra);
+}
+function mostrarEnCarrito(item) {
+    const listaCarrito = document.getElementById("listaCarrito");
+    const li = document.createElement("li");
+    li.textContent = `${item.modelo} - ${item.precio}`;
+    listaCarrito.appendChild(li);
+  }
 

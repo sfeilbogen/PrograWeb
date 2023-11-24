c3b52dd3246e03a753a1f799468e9eba39f8eb53
sfeilbogen <88330260+sfeilbogen@users.noreply.github.com>
2023-11-24 14:42:16 -0300

    cambioos

--- mi_script.js
+++ mi_script.js
@@ -1,33 +1,49 @@
-const compras=[]
+let compras=[]
 
-function AgregoCarrito(){
-    let compra={
-        modelo:document.getElementById("modelo1").textContent,
-        precio:document.getElementById("precio1").textContent
-    }
+
+function AgregoCarrito(numero) {
+    let compra = {
+        modelo: document.getElementById("modelo" + numero).textContent,
+        precio: document.getElementById("precio" + numero).textContent
+    };
     compras.push(compra);
     mostrarEnCarrito(compra);
 }
-function AgregoCarrito2(){
-    let compra={
-        modelo:document.getElementById("modelo2").textContent,
-        precio:document.getElementById("precio2").textContent
-    }
-    compras.push(compra);
-    mostrarEnCarrito(compra);
-}
-function AgregoCarrito3(){
-    let compra={
-        modelo:document.getElementById("modelo3").textContent,
-        precio:document.getElementById("precio3").textContent
-    }
-    compras.push(compra);
-    mostrarEnCarrito(compra);
-}
+
+
 function mostrarEnCarrito(item) {
-    const listaCarrito = document.getElementById("listaCarrito");
-    const li = document.createElement("li");
-    li.textContent = `${item.modelo} - ${item.precio}`;
-    listaCarrito.appendChild(li);
-  }
+    const tablaCarrito = document.getElementById("tablaCarrito");
+    const fila = tablaCarrito.insertRow();
+    const celdaModelo = fila.insertCell(0);
+    const celdaPrecio = fila.insertCell(1);
+    celdaModelo.textContent = item.modelo;
+    celdaPrecio.textContent = item.precio;
 
+     actualizarTotal();
+}
+
+function actualizarTotal() {
+    const tablaCarrito = document.getElementById("tablaCarrito");
+    const totalElement = document.getElementById("totalCarrito");
+
+    let total = 0;
+    for (let i = 0; i < tablaCarrito.rows.length; i++) {
+        const precioTexto = tablaCarrito.rows[i].cells[1].textContent.trim();
+        const precioNumerico = parseFloat(precioTexto.replace('$', '').replace(',', ''));
+        total += precioNumerico;
+        
+    }
+
+   
+    totalElement.textContent = `Total: $${total.toFixed(2)}`;
+}
+
+
+function vaciarCarrito() {
+    compras = []; 
+    const tablaCarrito = document.getElementById("tablaCarrito");
+    while (tablaCarrito.rows.length > 0) {
+        tablaCarrito.deleteRow(0);
+    }
+    actualizarTotal();
+}
